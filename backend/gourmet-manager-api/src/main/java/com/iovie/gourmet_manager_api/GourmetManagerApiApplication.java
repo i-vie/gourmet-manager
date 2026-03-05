4e926072927f4a4d34f39d34127e11eec30c5afb
i-vie <iolanda.pastorvieira@gmail.com>
2026-03-05 21:06:19 +0000

    chore: initialize Spring Boot project structure with dependencies

---
+++ backend/gourmet-manager-api/src/main/java/com/iovie/gourmet_manager_api/GourmetManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.iovie.gourmet_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetManagerApiApplication.class, args);
+	}
+
+}
---
+++ backend/gourmet-manager-api/src/test/java/com/iovie/gourmet_manager_api/GourmetManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iovie.gourmet_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
